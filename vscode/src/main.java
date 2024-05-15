3ca34a45b700fc4c6b9e53b3a922e479e751d337
italoAlcantara77 <testeaula6242@gmail.com>
2024-05-15 12:28:36 -0300

    primeiro commit

---
+++ vscode/src/main.java
@@ -0,0 +1,12 @@
+package vscode.src;
+
+import vscode.src.pages.apresentation;
+
+public class main{
+    public static void main(String[] args) {
+        //Chamar as funções aqui
+        apresentation mensagem = new apresentation();
+        
+        mensagem.exibirMensagem();
+    }
+}
---
+++ vscode/src/pages/apresentation.java
@@ -0,0 +1,7 @@
+package vscode.src.pages;
+
+public class apresentation {
+    public void exibirMensagem(){
+        System.out.println("Olá! seja bem vindo(a) a TecShop!");    
+    }    
+}
