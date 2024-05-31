a11ca8e1bbe6c153509021b1c2ac5f712db9b4a6
Italo Alcântara <italoalcantara6@gmail.com>
2024-05-30 21:30:21 -0300

    commit do Rodrigo

--- vscode/src/main.java
+++ vscode/src/main.java
@@ -44,6 +44,69 @@ public class main {
         }
         return returnOption;
     }
+    public static String[] billInformation() {
+        String[] data = new String[6];
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("Para prosseguirmos com a sua compra, necessitamos que informe alguns dados:\n");
+        System.out.println("\nInsira seu nome sem caracteres especiais.");
+        System.out.printf("Informe seu nome completo: ");
+        data[0] = input.nextLine();
+
+        while (data[0].matches("^[A-Z][a-zA-Z]{2,}( [a-zA-Z]+)*$") == false) {
+            System.err.printf("=-=-=-NOME INVÁLIDO-=-=-=\n");
+            System.out.printf("Insira seu nome completo novamente: ");
+            data[0] = input.nextLine();
+        }
+
+        System.out.printf("\nInforme seu CPF(xxx.xxx.xxx-xx): ");
+        data[1] = input.nextLine();
+
+        while (data[1].matches("^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$") == false) {
+            System.err.printf("=-=-=-CPF INVÁLIDO-=-=-=\n");
+            System.out.printf("Insira seu CPF novamente: ");
+            data[1] = input.nextLine();
+        }
+        
+        System.out.printf("\nInforme seu logradouro(Nome da rua e N°): ");
+        data[2] = input.nextLine();
+        
+        while(data[2].matches("^[A-Z][a-zA-Z ]+ N°\\d{1,4}$") == false){
+            System.err.printf("=-=-=-LOGRADOURO INVÁLIDO-=-=-=\n");
+            System.out.printf("Insira seu Logradouro novamente: ");
+            data[2] = input.nextLine();
+        }
+        
+        System.out.printf("\nInforme o nome de seu bairro: ");
+        data[3] = input.nextLine();
+        
+        while(data[3].matches("^[A-Z][a-zA-Z]{3,}( [a-zA-Z]+)*$") == false){
+            System.err.printf("=-=-=-BAIRRO INVÁLIDO-=-=-=\n");
+            System.out.printf("Insira seu bairro novamente: ");
+            data[3] = input.nextLine();
+        }
+        
+        System.out.printf("\nInforme seu CEP(xxxxx-xxx): ");
+        data[4] = input.nextLine();
+        
+        while(data[4].matches("^\\d{5}-\\d{3}$") == false){
+            System.err.printf("=-=-=-CEP INVÁLIDO-=-=-=\n");
+            System.out.printf("Insira seu CEP novamente: ");
+            data[4] = input.nextLine();
+        }
+        
+        System.out.printf("\nInforme sua cidade(Sao Paulo-SP): ");
+        data[5] = input.nextLine();
+        
+        while(data[5].matches("^[A-Z][a-zA-Z ]+-[A-Z]{2}") == false){
+            System.err.printf("=-=-=-CIDADE INVÁLIDA-=-=-=\n");
+            System.out.printf("Insira sua cidade novamente: ");
+            data[5] = input.nextLine();
+        }
+        
+        return data;
+    }
+
 
     public static void cart(){
         
